6bbbd0c6c9ba711940a07f189cedb9127890cda8
Prijo <prijoturner@gmail.com>
2018-06-20 14:14:54 +0700

    Converting to MVC design pattern

---
+++ todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  todoey
+//
+//  Created by Prijo on 20/06/18.
+//  Copyright © 2018 Prijo Turner. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
